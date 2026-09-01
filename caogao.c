eda252c0dc17a14ecc32d04e4e314b71ef4aabdf
5wu777 <1834560938@qq.com>
2026-09-01 23:57:15 +0800

    第一次版本提交

---
+++ caogao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char c1='a';
+    printf("%c\n",c1);
+    char c2='A';
+    printf("%c\n",c2);
+    char c3=',';
+    printf("%c\n",c3);
+    printf("%zu\n",sizeof(c1));
+}   
+
+int
+long
+long long
+short
+float
+double
---
+++ constant_exercise.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("我的名字是：%d\n性别%c，身高%fcm",57,',',175.0);
+    return 0;
+}
